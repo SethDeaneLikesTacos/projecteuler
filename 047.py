c97a7626531dbd7ac8d3ddf6518488c169222571
Seth Deane <sethdeane@Seths-MacBook-Pro.local>
2018-09-14 17:44:58 -0400

    Completed 047.py

---
+++ 047.py
@@ -0,0 +1,64 @@
+import pef
+import time
+"""
+The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 × 7
+15 = 3 × 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2² × 7 × 23
+645 = 3 × 5 × 43
+646 = 2 × 17 × 19.
+
+Find the first four consecutive integers to have four distinct prime factors
+each. What is the first of these numbers?
+"""
+
+
+def number_unique_prime_factors(number, primes):
+    not_completely_factored = True
+    i = 0
+    factors = []
+    while not_completely_factored:
+
+        if ( number / primes[i] == int(number / primes[i]) ):
+            number = number / primes[i]
+            factors.append(primes[i])
+            i = -1
+
+        if pef.isprime(number):
+            not_completely_factored = False
+            factors.append(int(number))
+        i += 1
+
+    return len(set(factors))
+
+
+def main():
+
+    how_many_primes = 500000
+    primes = list(pef.prime_sieve(0, how_many_primes))
+
+    amt_to_find = 4
+    i = amt_to_find
+
+    for number in range(5, how_many_primes):
+        num_primes = number_unique_prime_factors(number, primes)
+        # print(f"{number}, {num_primes}")
+
+        if (num_primes == amt_to_find):
+            i -= 1
+        else:
+            i = amt_to_find
+
+        if i == 0:
+            return number - amt_to_find + 1
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    answer = main()
+    end_time = time.time()
+    pef.answer(answer, end_time - start_time)
