4144e54de4032ff44b353bc9f20a52c5274458c5
Seth Deane <sethdeane@Seths-MacBook-Pro.local>
2018-09-14 17:48:18 -0400

    Modified some notes on 048.py

--- 048.py
+++ 048.py
@@ -1,9 +1,9 @@
 import pef
 import time
 """
-The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 
-Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 """
 
 
@@ -12,7 +12,6 @@ def main():
     for i in range(1,1001):
         total += i**i
     return str(total)[-10:]
-    # 9110846700
 
 
 if __name__ == "__main__":
