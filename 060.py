216d302682db8604ce1595998449b683ccb1d044
Seth Deane <sethdeane16@gmail.com>
2020-05-09 13:16:10 -0400

    made somes changes to 60. not done yet. very inefficient

--- 060.py
+++ 060.py
@@ -10,30 +10,39 @@ https://projecteuler.net/problem=60
 
 def main():
 
-    primes = pef.prime_sieve(700)
-    group_size = 4
-    prime_groups = combinations(primes, group_size) 
+    primes = pef.prime_sieve(50)
+    group_size = 3
+    prime_groups = combinations(primes, group_size)
+    print(list(prime_groups))
+    print(list(prime_groups)[1])
 
+    print("Done making combinations...")
     
     for prime_group in list(prime_groups):
-        prime_pairs = permutations(prime_group, 2)
 
+        # ignore 2 and 5, which would never create a possible solution
+        if (2 in prime_group) or (5 in prime_group):
+            continue
+
+        prime_pairs = permutations(prime_group, 2)
         count = 0
 
         for prime_pair in list(prime_pairs):
+
             concat_prime_pair = int(str(prime_pair[0]) + str(prime_pair[1]))
 
             if pef.is_prime(concat_prime_pair):
                 count += 1
 
-            if count == math.factorial(group_size):
-                total = 0
-                for i in range(group_size):
-                    total += prime_group[i]
-                return total
+        # 12 for group_size of 4 (4 pick 2 = 12)
+        # 20 for group_size of 5 (5 pick 2 = 20)
+        if count == 20:
+            return sum(prime_group)
 
-        
-    return "faksjh"
+        # print("count: " + str(count))
+        # print("sum: " + str(sum(prime_group)))
+        # if sum(prime_group) == 792:
+        #     break
 
 
 if __name__ == "__main__":
