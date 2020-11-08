e7d272a99ea8a538dacb204a1c24af325262755d
Seth Deane <sethdeane16@gmail.com>
2020-11-08 07:36:48 -0500

    Complete 071.py

---
+++ 071.py
@@ -0,0 +1,13 @@
+import resources.pef as pef
+import time
+import itertools
+"""
+https://projecteuler.net/problem=71
+
+"""
+
+# 3/7 = 0.42857142857
+# That means that 3/7 ~= 428571/1000000
+# So if you subtract 1 from the numerator you get, 428570.
+# Since that can't be simplified (when in fraction with 1000000),
+# that is the answer.
